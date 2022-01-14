09a6e768caffdc88610388ab376199f01ab9f151
VictorLeweke <80428278+VictorLeweke@users.noreply.github.com>
2022-01-14 12:31:47 +0100

    Changes to UNO

---
+++ SendDataUno/SendDataUno.ino
@@ -0,0 +1,37 @@
+#include <SoftwareSerial.h>
+
+const byte tempSensor = A0;
+const byte potSensor = A1;
+
+SoftwareSerial espSerial(5, 6);
+
+
+void setup() {
+  
+  Serial.begin(115200);
+  espSerial.begin(115200);
+
+}
+
+void loop() {
+
+  String temp = getTemp();
+  String potVal = getPot();
+  String str = temp+String("; ")+potVal;
+  espSerial.println(str);
+  delay(1000);
+
+  
+}
+
+String getTemp() {
+  float analogVal = analogRead(tempSensor);
+  float milliVolts = (analogVal / 1024.0) * 5000; // Convert to millivolts
+  int temp = milliVolts/10; // Convert to temperature
+  return String(temp);
+}
+
+String getPot() {
+  int potVal = analogRead(potSensor);
+  return String(potVal);
+}
