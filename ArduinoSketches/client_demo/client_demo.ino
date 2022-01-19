8ae3d72c02045ac6dbaae1ff8f489b04fa0c3bc0
WorldZage <augustfkv@hotmail.com>
2022-01-19 20:39:59 +0100

    comments/documentation client_demo.ino

--- ArduinoSketches/client_demo/client_demo.ino
+++ ArduinoSketches/client_demo/client_demo.ino
@@ -1,5 +1,9 @@
 #include "client_lib.h"
 
+// This sketch is purely for testing the wrapping functions of client_lib.h
+// it will run the POSTrequests and the GETrequests, and basically use all of the functions listed in the header.
+
+
 void setup() {
   Serial.begin(115200);
   Serial.println("");
@@ -15,13 +19,19 @@ void setup() {
 int mainDelay = 5 * 1000;
 int POSTdelay = 35 * 1000;
 void testPOSTfunction( void (*POSTfunc)(bool) ) {
+  // The post functions all have the same signature: return type of void, parameter of Bool.
+  // So it's easy to test them all;
+  // we create a function that takes a function with the aforementioned signature,
+  // and run it with the 2 possible parameter.
   (*POSTfunc)(true);
-  delay(POSTdelay * 3);
+  delay(POSTdelay * 3); // Delay between each posting, to give the webpage ample time to automatically update
   (*POSTfunc)(false);  
-  delay(POSTdelay);
+  delay(POSTdelay); // delay before going to the next test function.
 }
 
 void testGEThome() {
+  // Unlike the wrapped POST functions, the 2 GET functions have different signatures.
+  // Since C/C++ is a bit strict on typing, it's easier to just create hand-made testing functions for them.
   bool homeValue = GEThome();
   String homeText = homeValue ? "User is home" : "User is not home";
   Serial.println(homeText);
@@ -29,21 +39,22 @@ void testGEThome() {
 }
 
 void testGETwaitTime() {
+  // same comment as in testGEThome(), above.
   int waitTime = GETwaitTime();
   Serial.print("Waiting time is: "); Serial.println(waitTime);
   delay(POSTdelay);
 }
 
 void testCleanString(String input) {
+  // testing the cleanString function, to make sure it was cleaning the input Strings correctly.
   Serial.print("before cleaning: "); Serial.println(input);
   input = cleanString(input);
   Serial.print("after cleaning: "); Serial.println(input);
+  delay(POSTdelay);
 }
 
 void loop() {
-  delay(mainDelay);
-
-  // testCleanString("This is a string \" with quotes \" ");
+  testCleanString("This is a string \" with quotes \" ");
   
   // Testing the POST functions.
   testPOSTfunction(POSTfridge);
@@ -52,7 +63,6 @@ void loop() {
   testPOSTfunction(POSTshower);
   
   // Testing the GET functions.
-  
   testGEThome();
   testGETwaitTime(); 
   
