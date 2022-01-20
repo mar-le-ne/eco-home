003d27d7c2451a0b6690547f5c6ec1c53ab70647
VictorLeweke <80428278+VictorLeweke@users.noreply.github.com>
2022-01-20 16:39:41 +0100

    comments

--- ArduinoSketches/WaterSensor/WaterSensor.ino
+++ ArduinoSketches/WaterSensor/WaterSensor.ino
@@ -25,8 +25,8 @@ int timeCount = 0;           // Time counter
 
 void setup() {
   Serial.begin(115200);
-  String wName = "Basecamp Resident 2G";
-  String wPass = "8IndeedBerlinRock666";
+  String wName = "Wifi";
+  String wPass = "Pass";
   String IP = "172.16.129.160";
   setupWIFI(wName, wPass, IP);
   
